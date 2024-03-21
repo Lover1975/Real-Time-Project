91269a0fa51ca0a5dcc1d85bf1ae737db2edf1b5
Lover1975 <drjoenh1983@gmail.com>
2024-03-20 17:20:40 -0700

    Ready for main algorithm.

--- main.py
+++ main.py
@@ -38,28 +38,24 @@ class Resource:
         self.nestable = nestable
         self.nested_access = None
 
-    def __repr__(self):
-        if self.nestable and self.nested_access:
-            return f"Resource {self.resource_id} (Nestable, can access: Resource {self.nested_access.resource_id})"
-        elif self.nestable:
-            return f"Resource {self.resource_id} (Nestable, no nested access defined)"
-        else:
-            return f"Resource {self.resource_id} (Non-Nestable)"
+    # def __repr__(self):
+    #     if self.nestable and self.nested_access:
+    #         return f"Resource {self.resource_id} (Nestable, can access: Resource {self.nested_access.resource_id})"
+    #     elif self.nestable:
+    #         return f"Resource {self.resource_id} (Nestable, no nested access defined)"
+    #     else:
+    #         return f"Resource {self.resource_id} (Non-Nestable)"
 
 
 def create_resources(num_resources):
     created_resources = [Resource(i + 1, random.choice([True, False])) for i in range(num_resources)]
-    for resource in created_resources:
-        if resource.nestable:
-            potential_resources = [choose_res for choose_res in created_resources
-                                   if choose_res != resource and choose_res.nestable]
-            if potential_resources:
-                accessed_resource = random.choice(potential_resources)
-                resource.nested_access = accessed_resource
+    created_resources[0].nested_access = [created_resources[1], created_resources[2]]
+    created_resources[3].nested_access = [created_resources[4]]
     return created_resources
 
 
 class Task:
+
     def __init__(self, unique_id, arrival, execution, deadline, period=None):
         self.id = unique_id
         self.arrival = arrival
@@ -74,27 +70,38 @@ class Task:
         self.cpu_name = None
         self.state = 'preemptable'
 
-    def generate_critical_sections(self, nesting_factor, all_resources):
-        critical_sections_count = random.randint(0, min(8, int(self.execution)))
-        self.execution_timeline = [('Non-Critical', 0, None)] * int(self.execution)
-        critical_sections_positions = random.sample(range(int(self.execution)), critical_sections_count)
+    def check_valid_critical_assignment(self, position_index, nesting_degree_space):
+        for i in range(nesting_degree_space):
+            if self.execution_timeline[position_index + i][0] != 'Non-Critical':
+                return False
+        return True
 
-        for pos in critical_sections_positions:
-            nested_probability = random.random()
-            resource = random.choice(all_resources)
-            if (nested_probability < nesting_factor ** 2 and (pos + 2 < int(self.execution))
-                    and resource.nestable and resource.nested_access):
-                for q in all_resources:
-                    if q.resource_id == resource.nested_access and q.nested_access:
-                        self.execution_timeline[pos] = ('Critical', 2, resource.resource_id)
-                        self.execution_timeline[pos + 1] = ('Nested', 2, resource.nested_access)
-                        self.execution_timeline[pos + 2] = ('Nested', 2, q.nested_access)
-            elif (nested_probability < 2 * nesting_factor * (1 - nesting_factor) and (pos + 1 < int(self.execution))
-                  and resource.nestable and resource.nested_access):
-                self.execution_timeline[pos] = ('Critical', 1, resource.resource_id)
-                self.execution_timeline[pos + 1] = ('Nested', 1, resource.nested_access)
-            else:
-                self.execution_timeline[pos] = ('Critical', 0, resource.resource_id)
+    def generate_critical_sections(self, nesting_factor):
+        critical_sections_count = random.randint(0, 8)
+        self.execution_timeline = [('Non-Critical', 0, "None")] * int(self.execution)
+        if critical_sections_count > self.execution:
+            critical_sections_count = int(self.execution)
+        critical_sections_positions = random.sample(range(int(self.execution)), critical_sections_count)
+        print(critical_sections_positions)
+        for i in range(critical_sections_count):
+            for pos in critical_sections_positions:
+                nested_probability = random.random()
+                if (nested_probability < nesting_factor ** 2 and self.execution >= pos + 4
+                        and self.check_valid_critical_assignment(pos, 4)):
+                    self.execution_timeline[pos] = ('Critical', 2, "group1")
+                    self.execution_timeline[pos + 1] = ('Nested1', 2, "group1")
+                    self.execution_timeline[pos + 2] = ('Nested2', 2, "group1")
+                    self.execution_timeline[pos + 3] = ('Critical', 2, "group1")
+                elif nested_probability < 2 * nesting_factor * (1 - nesting_factor) and self.execution >= pos + 3\
+                        and self.check_valid_critical_assignment(pos, 3):
+                    self.execution_timeline[pos] = ('Critical', 1, "group2")
+                    self.execution_timeline[pos + 1] = ('Nested1', 1, "group2")
+                    self.execution_timeline[pos + 2] = ('NestedCritical', 1, "group2")
+                elif self.execution >= pos + 2\
+                        and self.check_valid_critical_assignment(pos, 2):
+                    chosen_group = random.randint(5, 10)
+                    self.execution_timeline[pos] = ('Critical', 0, f"group{chosen_group}")
+                    self.execution_timeline[pos + 1] = ('Critical', 0, f"group{chosen_group}")
 
     def is_runnable(self):
         return self.state in ['preemptable', 'non-preemptable']
@@ -105,8 +112,8 @@ class Task:
     @staticmethod
     def assign_priorities(tasks):
         sorted_tasks = sorted(tasks, key=lambda x: (x.deadline, x.id))
-        for priority, task in enumerate(sorted_tasks):
-            task.priority = priority + 1
+        for priority, c_task in enumerate(sorted_tasks):
+            c_task.priority = priority + 1
         return sorted_tasks
 
     def __repr__(self):
@@ -120,10 +127,11 @@ def UUniFast(n, u_bar):
     sum_u = u_bar
     vect_u = np.zeros(n)
     for i in range(n - 1):
-        next_sum_u = sum_u * (1 - (np.random.rand() ** (1.0 / (n - i))))
+        next_sum_u = sum_u * (np.random.rand() ** (1.0 / (n - i)))
         vect_u[i] = sum_u - next_sum_u
         sum_u = next_sum_u
     vect_u[n - 1] = sum_u
+    print(vect_u)
     return vect_u
 
 
@@ -131,10 +139,10 @@ def generate_tasks(num_tasks, u_bar):
     utilizations = UUniFast(num_tasks, u_bar)
     tasks = []
     for i in range(num_tasks):
-        arrival = np.random.randint(0, 100)
-        max_execution_time = 100
+        arrival = np.random.randint(0, 300)
+        max_execution_time = 500
         execution = np.ceil(utilizations[i] * max_execution_time)
-        buffer = np.random.randint(1, 50)
+        buffer = np.random.randint(5, 250)
         deadline = arrival + execution + buffer
         tasks.append(Task(f"Task_{i + 1}", arrival, execution, deadline))
     return tasks
@@ -152,8 +160,8 @@ resources = create_resources(10)
 for res in resources:
     print(res)
 f = 0.05
-for task in generated_tasks[:5]:
-    task.generate_critical_sections(f, resources)
+for task in generated_tasks[:100]:
+    task.generate_critical_sections(f)
     print(task)
 processors = [Processor(i) for i in range(1, 5)]
 print(processors)
